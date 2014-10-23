7ac52b3049894e2f02bc3c9b62a5e6a6cfba5be9
zhitong.zhang <zztztt109@gmail.com>
2014-10-23 11:09:42 +0800

    Path Sum

---
+++ leetcode/PathSum/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+ *
+ */
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+  public:
+    bool hasPathSum(TreeNode *root, int sum) {
+      if (root == NULL)
+        return false;
+      if (root->left == NULL && root->right == NULL && sum - root->val == 0)
+       return true; 
+      return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+    }
+};
